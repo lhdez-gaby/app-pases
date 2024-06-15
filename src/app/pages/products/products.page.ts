f5867fead9544bb8ccf25784cd4807553256221c
Gabriela López <lhdez.gaby@gmail.com>
2024-06-15 15:05:03 -0600

    feat: add product list mode on product page

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonThumbnail, IonContent, IonRow ,IonCol, IonImg, IonCard, IonLabel, IonText } from '@ionic/angular/standalone';
+import { IonThumbnail, IonContent, IonRow ,IonCol, IonImg, IonCard, IonLabel, IonText, IonList, IonItem } from '@ionic/angular/standalone';
 import { ApiService } from 'src/app/services/api/api.service';
 import { Product } from 'src/app/models/product.model';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -19,7 +19,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
     IonImg, 
     IonCard,
     IonLabel,
-    IonText, 
+    IonText,
+    IonList, 
+    IonItem,
     CommonModule, 
     FormsModule, 
     SharedModule,]
@@ -30,6 +32,7 @@ export class ProductsPage implements OnInit {
   // Se agregan dos arreglos de productos, una para guardar permanentemente todos los productos y otra para que cambie en función de la búsqueda.
   products: Product[] = [];
   allProducts: Product[] = [];
+  showGrid: boolean = false;
 
   ngOnInit() {
     this.getProducts();
