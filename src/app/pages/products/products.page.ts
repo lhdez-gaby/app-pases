a47ddd67e14bafbeb9ec8016835c134b073d47c2
Gabriela López <lhdez.gaby@gmail.com>
2024-06-15 16:11:16 -0600

    feat: add search bar on product page

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonThumbnail, IonContent, IonRow ,IonCol, IonImg, IonCard, IonLabel, IonText, IonList, IonItem } from '@ionic/angular/standalone';
+import { IonThumbnail, IonContent, IonRow ,IonCol, IonImg, IonCard, IonLabel, IonText, IonList, IonItem,  IonTitle, IonToolbar,IonSearchbar } from '@ionic/angular/standalone';
 import { ApiService } from 'src/app/services/api/api.service';
 import { Product } from 'src/app/models/product.model';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -22,6 +22,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
     IonText,
     IonList, 
     IonItem,
+    IonTitle,
+    IonToolbar,
+    IonSearchbar,
     CommonModule, 
     FormsModule, 
     SharedModule,]
@@ -32,7 +35,9 @@ export class ProductsPage implements OnInit {
   // Se agregan dos arreglos de productos, una para guardar permanentemente todos los productos y otra para que cambie en función de la búsqueda.
   products: Product[] = [];
   allProducts: Product[] = [];
+
   showGrid: boolean = false;
+  query!:string;
 
   ngOnInit() {
     this.getProducts();
@@ -41,5 +46,25 @@ export class ProductsPage implements OnInit {
   getProducts(){
     this.allProducts = this.api.items;
     this.products = [...this.allProducts];
+  }
+  // ----------Manejo de búsque de productos----------
+  onSearchChange(event: any){
+    this.query = event.detail.value.toLowerCase().normalize('NFD');
+    this.querySearch(); 
+  }
+
+  querySearch(){
+    this.products = [];
+    if (this.query.length > 0){
+      this.searchItems();
+    }else{
+      this.products = [...this.allProducts];
+    }
+  }
+
+  searchItems(){
+    this.products = this.api.items.filter((product) =>
+      product.productName.toLowerCase().normalize('NFD').includes(this.query)
+    );
   }
 }
