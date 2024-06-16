4f4a9c985faaf5bb6e69b546ed7acca59dc04eec
Gabriela López <lhdez.gaby@gmail.com>
2024-06-16 00:33:27 -0600

    feat: incorporate add to cart button on product detail page

--- src/app/pages/products/product-detail/product-detail.page.ts
+++ src/app/pages/products/product-detail/product-detail.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, NavController, IonImg, IonItem, IonLabel, IonText } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, NavController, IonImg, IonItem, IonLabel, IonText, IonFooter, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api/api.service';
 import { Product } from 'src/app/models/product.model';
@@ -23,6 +23,9 @@ import { Product } from 'src/app/models/product.model';
     IonItem,
     IonLabel,
     IonText,
+    IonFooter,
+    IonButton,
+    IonIcon,
     CommonModule, 
     FormsModule]
 })
@@ -30,11 +33,11 @@ export class ProductDetailPage implements OnInit {
 
   id?: string;
   product?: Product;
+  addToCart?: string | null;
 
   private route = inject(ActivatedRoute);
   private navControl = inject(NavController);
   private api = inject(ApiService);
-   
 
   constructor() { }
 
@@ -51,6 +54,18 @@ export class ProductDetailPage implements OnInit {
 
     this.id = id;
     this.product = this.api.items.find((item)=> item.id == id);
+  }
+
+  addProduct(){
+    this.addedText();
+  }
+
+  addedText(){
+    this.addToCart = "Agregado al carrito";
+    setTimeout(()=>{
+      this.addToCart = null;
+    },1000
+    );
   }
 
 }
