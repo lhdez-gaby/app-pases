d889a2df4140680a4cf852d2545c1a747bfe690d
Gabriela López <lhdez.gaby@gmail.com>
2024-06-17 18:31:29 -0600

    refactor: add custom-input component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,16 +5,19 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterLink } from '@angular/router';
 import { BackButtonComponent } from './components/back-button/back-button.component';
+import { CustomInputComponent } from './components/custom-input/custom-input.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     BackButtonComponent,
+    CustomInputComponent,
   ],
   exports: [
     HeaderComponent,
     BackButtonComponent,
+    CustomInputComponent,
   ],
   imports: [
     CommonModule,
