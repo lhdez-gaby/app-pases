b199fe5be1ed897fba5e8906e9851f7c2ef81535
Gabriela López <lhdez.gaby@gmail.com>
2024-06-18 19:08:11 -0600

    feature: update header on my passes page

--- src/app/pages/mypasses/mypasses.page.ts
+++ src/app/pages/mypasses/mypasses.page.ts
@@ -2,13 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-mypasses',
   templateUrl: './mypasses.page.html',
   styleUrls: ['./mypasses.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    CommonModule, 
+    FormsModule,
+    SharedModule,
+  ]
 })
 export class MypassesPage implements OnInit {
 
